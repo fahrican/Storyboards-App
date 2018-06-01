6c4d8e1e6e351a10ae29d401b797dc9663023015
Fahri MacBook <fahri-can@web.de>
2018-06-01 14:38:57 +0200

    Add player model.

---
+++ RatingsTutorial/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  RatingsTutorial
+//
+//  Created by fahri on 01.06.18.
+//  Copyright © 2018 fahri. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var name: String?
+    var game: String?
+    var rating: Int
+}
