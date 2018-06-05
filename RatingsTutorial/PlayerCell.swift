5fa276fb5b9e3ed7b565eadb4a0fed4fe3ac9b22
Fahri MacBook <fahri-can@web.de>
2018-06-06 00:04:29 +0200

    Use of subclass for the cell.

---
+++ RatingsTutorial/PlayerCell.swift
@@ -0,0 +1,31 @@
+//
+//  PlayerCell.swift
+//  RatingsTutorial
+//
+//  Created by fahri on 05.06.18.
+//  Copyright © 2018 fahri. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    var player: Player? {
+        didSet {
+            guard let player = player else { return }
+            
+            gameLabel.text = player.game
+            nameLabel.text = player.name
+            ratingImageView.image = image(forRating: player.rating)
+        }
+    }
+    
+    func image(forRating rating: Int) -> UIImage? {
+        let imageName = "\(rating)Stars"
+        return UIImage(named: imageName)
+    }
+}
