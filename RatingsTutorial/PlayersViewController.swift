346477096a6b5bf27f2ba1d1165eae58e61113ea
Fahri MacBook <fahri-can@web.de>
2018-07-04 23:38:53 +0200

    Enable to edit player name.

--- RatingsTutorial/PlayersViewController.swift
+++ RatingsTutorial/PlayersViewController.swift
@@ -31,6 +31,20 @@ extension PlayersViewController {
         tableView.insertRows(at: [indexPath], with: .automatic)
     }
     
+    @IBAction func saveToMainViewController (segue:UIStoryboardSegue) {
+        
+        let detailViewController = segue.source as! DetailTableViewController
+        
+        let index = detailViewController.index
+        
+        let modelString = detailViewController.editedModel
+        
+        players[index!].name = modelString!
+        
+        tableView.reloadData()
+        
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return players.count
@@ -44,5 +58,22 @@ extension PlayersViewController {
         let player = players[indexPath.row]
         cell.player = player
         return cell
+    }
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "edit" {
+            
+            var path = tableView.indexPathForSelectedRow
+            
+            var detailViewController = segue.destination as! DetailTableViewController
+            
+            detailViewController.index = path?.row
+            detailViewController.modelArray = players
+            
+        }
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
     }
 }
